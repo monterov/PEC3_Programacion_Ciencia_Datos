657fb573102cc5d06563eba5062470eab967dd21
Miguel Ángel Montero <miguelangelmontero.valero@gmail.com>
2025-12-19 16:03:53 +0100

    Implement optimized factorial sum function
    Added a new optimized factorial sum function.

--- ejercicio_3/Ejercicio_3.py
+++ ejercicio_3/Ejercicio_3.py
@@ -1,1 +1,38 @@
+def factorial_sum(n: int):
+    """Returns the sum of the factorials of the first n natural numbers.
 
+    Args:
+        n (int): Positive integer indicating the upper limit of the factorial sum.
+
+    Returns:
+        (int): Sum of factorials from 1! to n!.
+    """
+    total = 0
+    for i in range(1, n + 1):
+        fact = 1
+        for j in range(1, i + 1):
+            fact *= j
+        total += fact
+    return total
+
+# factorial_sum(3500)
+
+%timeit factorial_sum(3500)
+
+%lprun -f factorial_sum factorial_sum(3500)
+
+def factorial_sum_mejorada(n: int) -> int:
+    """Returns the sum of the factorials of the first n natural numbers."""
+    total = 0
+    fact = 1
+
+    for i in range(1, n + 1): # Como ya tenemos aquí el factorial...
+        fact *= i #...lo aprovechamos aquí 
+        total += fact
+
+    return total
+
+
+%timeit factorial_sum_mejorada(3500)
+
+%lprun -f factorial_sum_mejorada factorial_sum_mejorada(3500)
